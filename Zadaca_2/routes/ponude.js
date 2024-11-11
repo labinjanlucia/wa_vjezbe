931ae2689c6b1a4d0776e1da2150dd32603a9631
labinjanlucia <134169024+labinjanlucia@users.noreply.github.com>
2024-11-11 20:11:38 +0100

    Update ponude.js
    dodala provjere u ponude.js

--- Zadaca_2/routes/ponude.js
+++ Zadaca_2/routes/ponude.js
@@ -2,10 +2,12 @@ import express from 'express';
 const router = express.Router();
 
 const ponude=[];
+//prikazivanje ponuda
 
 router.get('/ponude', (req, res) => {
     res.json(ponude);
 });
+//ponude id provjer broja i id
 router.get('/ponude/:id', (req, res) => {
     const id_ponuda = parseInt(req.params.id);
     if (isNaN(id_ponuda)) {
@@ -19,19 +21,30 @@ router.get('/ponude/:id', (req, res) => {
         res.status(404).json({ message: "Ponuda s traženim ID-em ne postoji." });
     }
 });
+
 router.post('/ponude', (req, res) => {
     const { id, ime, prezime, ponudena_cijena, broj_telefona } = req.body;
 
-});
+
+//jeli sve uneseno
 
 if (!id || !ime|| !prezime || ponudena_cijena == null || !broj_telefona) {
     return res.status(400).json({ message: "Unesite sve podatke" });
 }
-
+//provjera id nekretnine
 const postoji_nekretnina = nekretnine.some(nekretnina => nekretnina.id === id_nekretnine);
     if (!postoji_nekretnina) {
         return res.status(404).json({ message: "Nekretnina s traženim ID-em ne postoji." });
     }
+//provjerit jesu li podaci ispravni
+if (typeof ponudena_cijena !== 'number' || ponudena_cijena < 0) {
+    return res.status(400).json({ message: "Ponudena cijena mora biti pozitivan broj." });
+}
+if (typeof ime !== 'string' || typeof prezime !== 'string') {
+    return res.status(400).json({ message: "Ime i prezime moraju biti u obliku podaci." });
+}
+
+
     const nova_ponuda = {
         id: ponude.length + 1,
         id,
@@ -41,3 +54,5 @@ const postoji_nekretnina = nekretnine.some(nekretnina => nekretnina.id === id_ne
         broj_telefona
     };
     ponude.push(nova_ponuda);
+
+});
