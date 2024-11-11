37ca49fa58a12ddb26e02440e75e95e783379888
labinjanlucia <134169024+labinjanlucia@users.noreply.github.com>
2024-11-11 20:31:34 +0100

    Update nekretnine.js
    dodala nekretnine.js dodavanje nove nekretnine i provjere

--- Zadaca_2/routes/nekretnine.js
+++ Zadaca_2/routes/nekretnine.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 
 const nekretnine = [
-{id:1,naziv:"Kuca_s_bazenom",opis:"Kuca u uvali s bazenom"}
+{id:1,naziv:"Kuca_s_bazenom",opis:"Kuca u uvali s bazenom", cijena:50000,lokacija:"Medulin",broj_soba:4,povrsina:3000},
+{id:2,naziv:"Kuca_kraj_mora",opis:"Kuca 500m od mora", cijena:100000,lokacija:"Stinjan",broj_soba:3,povrsina:2000},
+{id:1,naziv:"Kuca_u_centru",opis:"Kuca u centru grada", cijena:30000,lokacija:"Medulin",broj_soba:2,povrsina:200}
 ];
 
 
@@ -24,22 +26,27 @@ router.get('/nekretnine/:id', (req, res) => {
         res.json(nekretnine);
     }
     else{
-        res.json({message:"Nekretnina s trazenim id-om ne postoji"});
+        res.status(404).json({message:"Nekretnina s trazenim id-om ne postoji"});
     }
     });
 
-    router.delete('/:id', (req, res) => {
-        const id_nekretnina = parseInt(req.params.id); // Parse ID as an integer
-        const index = nekretnine.findIndex(nekretnina => nekretnina.id === id_nekretnina);
+
+router.post('/nekretnine', (req, res) => {
+    const { naziv, opis, cijena, lokacija, brojSoba, povrsina } = req.body;
+    });
+      
+router.delete('/:id', (req, res) => {
+    const id_nekretnina = parseInt(req.params.id); // Parse ID as an integer
+    const index = nekretnine.findIndex(nekretnina => nekretnina.id === id_nekretnina);
     
-        if (index !== -1) {
+    if (index !== -1) {
             
-            nekretnine.splice(index, 1);
-            res.json({ message: 'Nekretnina uspješno obrisana.' });
-        } else {
+        nekretnine.splice(index, 1);
+        res.json({ message: 'Nekretnina uspješno obrisana.' });
+    } else {
             
-            res.json({ message: 'Nekretnina s traženim ID-em ne postoji.' });
-        }
+        res.status(404).json({ message: 'Nekretnina s traženim ID-em ne postoji.' });
+    }
     });
     
 
