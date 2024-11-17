4d904f0461e756bfbe1e2be2192def729177a666
labinjanlucia <134169024+labinjanlucia@users.noreply.github.com>
2024-11-17 20:18:25 +0100

    testirala post funkcije narudzbe

--- webshop/server/index.js
+++ webshop/server/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 const app = express();
+
+
+
 app.use(express.json());
+
 const PORT = 3002;
 app.get('/', (req, res) => {
 res.send('Webshop API');
 });
 import proizvodiRouter from './routes/proizvodi.js';
 app.use('/proizvodi', proizvodiRouter);
+import narudzbeRouter from  './routes/narudzbe.js';
+app.use('/narudzbe', narudzbeRouter);
 app.listen(PORT, error => {
 if (error) {
 console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
--- webshop/server/routes/proizvodi.js
+++ webshop/server/routes/proizvodi.js
@@ -1,18 +1,7 @@
 import express from 'express';
 const router = express.Router();
-function Proizvod(id, naziv, cijena, velicine) {
-this.id = id;
-this.naziv = naziv;
-this.cijena = cijena;
-this.velicine = velicine;
-}
-const proizvodi = [
-new Proizvod(1, 'Obična crna majica', 100, ['XS', 'S', 'M', 'L']),
-new Proizvod(2, "Levi's 501 traperice", 110, ['S', 'M', 'L']),
-new Proizvod(3, 'Zimska kapa', 40, 'onesize'),
-new Proizvod(4, 'Čarape Adidas', 20, ['34-36', '37-39', '40-42']),
-new Proizvod(5, 'Tenisice Nike', 200, ['38', '39', '40', '41', '42', '43', '44', '45'])
-];
+import{proizvodi} from "../data.js"
+
 router.get('/', (req, res) => {
 res.status(200).json(proizvodi);
 });
