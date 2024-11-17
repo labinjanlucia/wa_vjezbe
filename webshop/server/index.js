3c842fb94f1440b56f4c568f4f019bed6b679dd9
labinjanlucia <134169024+labinjanlucia@users.noreply.github.com>
2024-11-17 19:35:27 +0100

    tested GET

--- webshop/server/index.js
+++ webshop/server/index.js
@@ -5,6 +5,41 @@ const PORT = 3002;
 app.get('/', (req, res) => {
 res.send('Webshop API');
 });
+
+function Proizvod(id, naziv, cijena, velicine) {
+    this.id = id;
+    this.naziv = naziv;
+    this.cijena = cijena;
+    this.velicine = velicine;
+    }
+    const proizvodi = [
+    new Proizvod(1, 'Obična crna majica', 100, ['XS', 'S', 'M', 'L']),
+    new Proizvod(2, "Levi's 501 traperice", 110, ['S', 'M', 'L']),
+    new Proizvod(3, 'Zimska kapa', 40, 'onesize'),
+    new Proizvod(4, 'Čarape Adidas', 20, ['34-36', '37-39', '40-42']),
+    new Proizvod(5, 'Tenisice Nike', 200, ['38', '39', '40', '41', '42', '43', '44', '45'])
+    ];
+
+    app.get('/proizvodi', (req, res) => {
+        res.status(200).json(proizvodi);
+        });
+        app.get('/proizvodi/:id', (req, res) => {
+        const id_req = req.params.id;
+        if (isNaN(id_req)) {
+        res.status(400).send('ID mora biti broj');
+        return;
+        }
+        const proizvod = proizvodi.find(proizvod => proizvod.id == id_req);
+        if (proizvod) {
+        
+        
+        res.status(200).json(proizvod);
+        } else {
+        res.status(404).send('Proizvod nije pronađen');
+        }
+        });
+
+
 app.listen(PORT, error => {
 if (error) {
 console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
